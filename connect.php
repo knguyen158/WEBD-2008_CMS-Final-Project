411c199e97dd5729e6d287198a34714cf01a67a1
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-07 19:03:25 -0600

    Set up the connection to database

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-    define('DB_DSN','mysql:host=localhost;dbname=serverside');
-    define('DB_USER','admin');
-    define('DB_PASS','P@ssword01');
+    define('DB_DSN','mysql:host=localhost;dbname=sfinalproject');
+    define('DB_USER','localhost');
+    define('DB_PASS','Password01');
 
     try {
         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
