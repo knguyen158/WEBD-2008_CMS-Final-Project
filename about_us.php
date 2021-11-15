551d0f0d46180d769873f6265d52008627ce0007
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-15 09:14:18 -0600

    Adds about us page for contact information

---
+++ about_us.php
@@ -0,0 +1,19 @@
+<?php 
+  require('connect.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Welcome to Green Bean shop!</title>
+</head>
+<body>
+  <?php include('header.php') ?>
+  <h1>Thank you for visiting us!</h1>
+  <p>Our office: 160 Princess St, Winnipeg, MB R3B 1K9</p>
+  <p>Our phone: (204) 632-3960</p>
+  <?php include('footer.php') ?>
+</body>
+</html>
