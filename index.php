09363b86c28517d55c806aaad92701465adc8bbd
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-15 09:11:35 -0600

    Adds brief information about the client to the home page

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-<?php
-  /*
-  This is the index page
-  */
-  
+<?php 
   require('connect.php');
-
-  // Retrieves rows from database
-  $query = "SELECT * FROM product ORDER BY date_created DESC LIMIT 9";
-
-  $statement = $db->prepare($query);
-
-  $statement->execute();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Welcome to Green Bean shop!</title>
+</head>
+<body>
+  <?php include('header.php') ?>
+  <h1>Hello, welcome to our eco-friendly shop!</h1>
+  <p>Located in Winnipeg, Green Bean has been delivering awesome products to help reduce clothing waste since 2020!</p>
+  <p>For each product that we sell, we will plant a baby tree in Assiniboine Forest. Giving back to community is our ultimate goal.</p>
+  <?php include('footer.php') ?>
+</body>
+</html>
