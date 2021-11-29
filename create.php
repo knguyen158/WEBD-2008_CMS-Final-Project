0086559966cf105d8b4b4c8bf93254b94b377319
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-28 21:22:06 -0600

    Adds css and styling to header and footer when loading the page.

--- create.php
+++ create.php
@@ -5,13 +5,13 @@
 
 	require('admin.php');
 	require('connect.php');
+	
+	// Retrieves rows from database
+	$query = "SELECT * FROM category ORDER BY name ASC";
 
-  // Retrieves rows from database
-  $query = "SELECT * FROM category ORDER BY name ASC";
+	$statement = $db->prepare($query);
 
-  $statement = $db->prepare($query);
-
-  $statement->execute();
+	$statement->execute();	
 ?>
 
 <!DOCTYPE html>
@@ -21,12 +21,13 @@
     <title>Publish a new product</title>
     <link rel="stylesheet" href="style.css" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://cdn.tiny.cloud/1/bli9ucpqopmfpf70el7yrbxzomw7la2a6cwrsj7mcah9gr8c/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 </head>
 <body>
 	<?php include('header.php') ?>
     <div>
 		<div>
-			<form action="process_product.php" method="post">
+			<form action="process_product.php" method="post" enctype='multipart/form-data'>
 		    	<fieldset>
 		      		<legend>Publish New Product</legend>
 		      			<p>						
@@ -47,14 +48,29 @@
 	      			</p>
 	      			<p>
 	        			<label for="product_price">Price</label>
-	        			<input type="number" step="any" name="product_price" id="product_price"></input>
+	        			<input type="number" step="any" name="product_price" id="product_price">
 	      			</p>
-		      			<p>
-		        			<input type="submit" name="command" value="Publish Product" />
-		      			</p>
+	      			<div>
+	      				<label for='file'>Filename:</label>
+	         			<input type='file' name='file' id='file'>
+	      			</div>	      			
+	      			<p>
+	        			<input type="submit" name="command" value="Publish Product" />
+	      			</p>
 		    	</fieldset>
 			</form>
 		</div>
     <?php include('footer.php') ?>
+    <script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+      toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Khanh Nguyen',      
+      forced_root_block : false,
+    });
+  </script>
 </body>
 </html>
