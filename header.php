8360cda132663a423a6bd00c200a8a43e52295db
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-28 21:24:05 -0600

    Adds search bar to appear on all pages.

--- header.php
+++ header.php
@@ -4,33 +4,40 @@
     */
 
   // Retrieves rows from database
-  $select_query = "SELECT * FROM category";
+  $select_query = "SELECT * FROM category ORDER BY name ASC";
 
   $cat_statement = $db->prepare($select_query);
 
   $cat_statement->execute();
+
+  $cat_row = $cat_statement->fetchAll();
 ?>
 
 <header>
   <div class="navbar">
-    <!-- Your page navigation -->
   <a href="index.php">Home</a>
   <a href="product.php">Products</a>
   <div class="dropdown">
     <button class="dropbtn">Shop By Category 
       <i class="fa fa-caret-down"></i>
     </button>
-  <div class="dropdown-content">
-    <?php while ($cat_row = $cat_statement->fetch()): ?>
-    <a href="selected_category.php?category_id=<?= $cat_row['id'] ?>"><?= $cat_row['name'] ?></a>
-  <?php endwhile ?>
-    <!-- <a href="skirts.php">Skirts</a>
-    <a href="pants.php">Pants</a>
-    <a href="underwear.php">Underwear</a>
-    <a href="dresses.php">Dresses</a>
-    <a href="accessories.php">Accessories</a> -->
-  </div>
+    <div class="dropdown-content">
+      <?php foreach ($cat_row as $item): ?>
+      <a href="selected_category.php?category_id=<?= $item['id'] ?>"><?= $item['name'] ?></a>
+      <?php endforeach ?>
+    </div>
   </div>
   <a href="about_us.php">About Us</a>
 </div>
+<a href="login.php">Login</a>
+<a href="register.php">Sign Up</a>
+<form action="product.php" method="get">
+  <input type="text" name="search_bar">
+  <select name="product_category" id="product_category">    
+    <option value="">All Products</option>
+    <?php foreach ($cat_row as $item): ?>
+    <option value="<?= $item['id'] ?>"><?= $item['name'] ?></option>
+    <?php endforeach ?>
+  </select> 
+</form>
 </header>  
