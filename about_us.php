c82eedd888963a30a65fa3667a4d4c240b9ff09e
knguyen158 <77942126+knguyen158@users.noreply.github.com>
2021-11-28 21:21:42 -0600

    Adds css and styling to header and footer when loading the page.

--- about_us.php
+++ about_us.php
@@ -8,6 +8,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Welcome to Green Bean shop!</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
   <?php include('header.php') ?>
